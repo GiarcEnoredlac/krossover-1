c485d121757eea05493fe6a22b0c93be2f1b52de
Michael McDevitt <mmcdevi1@gmail.com>
2015-03-20 18:02:38 -0400

    conversations controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
 
+  resources :conversations
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320214714) do
+ActiveRecord::Schema.define(version: 20150320215633) do
 
   create_table "admins", force: true do |t|
     t.string   "full_name"
@@ -91,6 +91,17 @@ ActiveRecord::Schema.define(version: 20150320214714) do
     t.integer "sash_id"
     t.string  "category", default: "default"
   end
+
+  create_table "messages", force: true do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "reviews", force: true do |t|
     t.string   "title"
