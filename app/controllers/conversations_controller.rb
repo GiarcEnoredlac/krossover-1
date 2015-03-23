6ced853a1a95b1dd3c84c29c6ddfa21d34d854bd
Michael McDevitt <mmcdevi1@gmail.com>
2015-03-23 17:44:17 -0400

    edit

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,3 +1,14 @@
 class ConversationsController < ApplicationController
 	before_action :authenticate_user!
+
+	def create
+    if Conversation.between(params[:sender_id],params[:recipient_id]).present?
+      @conversation = Conversation.between(params[:sender_id],params[:recipient_id]).first
+    else
+      @conversation = Conversation.create!(conversation_params)
+    end
+ 
+    render json: { conversation_id: @conversation.id }
+  end
+  
 end
