14035cb84db25f8ab69dfe3abaafd07e754326be
Michael McDevitt <mmcdevi1@gmail.com>
2015-03-28 13:44:44 -0400

    css leaderboards

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -2,7 +2,6 @@ class LeaderboardsController < ApplicationController
 
 	def index
 		@users = User.joins(:reviews).group('reviews.user_id').order("count(reviews.user_id) DESC")
-		# @featured_review = User.featured.reviews_count
 	end
 
 end
