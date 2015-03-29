cc973a86ff2ffb28519489cbb2d0551b2e1f5799
Michael McDevitt <mmcdevi1@gmail.com>
2015-03-29 11:36:15 -0400

    edit

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,7 +1,9 @@
 class LeaderboardsController < ApplicationController
 
 	def index
-		@users = User.joins(:reviews).group('reviews.user_id, users.id').order("count(reviews.user_id) DESC")
+		@users = User.joins(:reviews).select("users.*").group('reviews.user_id').order("count(reviews.user_id) DESC")
 	end
 
 end
+# @albums = Album.joins(:reviews).select("*, avg(reviews.rating) as average_rating").group("albums.id").order("average_rating DESC")
+# @albums = Album.joins(:reviews).select("albums.*, avg(reviews.rating) as average_rating").group("albums.id").order("average_rating DESC")
