967befff7e0b2c382a1998e33eb185f176d89144
Michael McDevitt <mmcdevi1@gmail.com>
2015-03-29 11:28:41 -0400

    edit

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,7 +1,7 @@
 class LeaderboardsController < ApplicationController
 
 	def index
-		@users = User.joins(:reviews).group('reviews.user_id').order("count(reviews.user_id) DESC")
+		@users = User.joins(:reviews).group('reviews.user_id, users.id').order("count(reviews.user_id) DESC")
 	end
 
 end
