ca9f09ec5621702e542ab1cebd55612dbcec0487
Michael McDevitt <mmcdevi1@gmail.com>
2015-03-31 15:25:16 -0400

    stuff

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Krossover
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += ['jquery.easypiechart.fill.js']
     config.middleware.delete Rack::Lock
     config.middleware.use FayeRails::Middleware, extensions: [CsrfProtection.new], mount: '/faye', :timeout => 25
 
